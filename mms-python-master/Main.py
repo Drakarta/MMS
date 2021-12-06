6f5f236b8d5ba3124b9ae8388209498937149bd4
Drakarta <Drakarta3@gmail.com>
2021-12-06 10:14:31 +0100

    Anthony

--- mms-python-master/Main.py
+++ mms-python-master/Main.py
@@ -5,17 +5,42 @@ def log(string):
     sys.stderr.write("{}\n".format(string))
     sys.stderr.flush()
 
+class MazeMapper:
+    MazeSize = [API.mazeWidth(), API.mazeHeight()]
+    Finish = [
+            [int(MazeSize[0] / 2), int(MazeSize[1] / 2)], 
+            [int(MazeSize[0] / 2 - 1), int(MazeSize[1] / 2)], 
+            [int(MazeSize[0] / 2), int(MazeSize[1] / 2 - 1)],
+            [int(MazeSize[0] / 2 - 1), int(MazeSize[1] / 2 - 1)]
+        ]
+    Map = [
+            []
+        ]
+    def MazeSetup():
+        API.setText(0, 0, "abc")
+        for pos in MazeMapper.Finish:
+            API.setColor(pos[0], pos[1], "A")
+            API.setText(pos[0], pos[1], "END")
+
+    def Mapper():
+        # 0 = No walls
+        # 1 = north wall
+        # 2 = east wall
+        # 4 = south wall
+        # 8 = west wall
+        pass
+
 class Movement:
     Compas = ["North", "East", "South", "West"]
-    Position = [1, 1]
+    Position = [0, 0]
     Direction = 0
     History = []
     Last = "Still"
     def Forward():
         if tuple(Movement.Position) not in Movement.History:
-            API.setColor(Movement.Position[0] - 1, Movement.Position[1] - 1, "G")
+            API.setColor(Movement.Position[0], Movement.Position[1], "G")
         else:
-            API.setColor(Movement.Position[0] - 1, Movement.Position[1] - 1, "Y")
+            API.setColor(Movement.Position[0], Movement.Position[1], "Y")
         Movement.History.append(tuple(Movement.Position))
         API.moveForward()
         if Movement.Direction == 0:
@@ -26,7 +51,6 @@ class Movement:
             Movement.Position[1] -= 1
         elif Movement.Direction == 3:
             Movement.Position[0] -= 1
-        
         Movement.Last = "Forward"
 
     def Left():
@@ -34,9 +58,8 @@ class Movement:
         Movement.Direction = Movement.Direction - 1
         if Movement.Direction < 0:
             Movement.Direction += 4
-        log(Movement.Last)
         if Movement.Last == "Left":
-            API.setColor(Movement.Position[0] - 1, Movement.Position[1] - 1, "Y")
+            Movement.History.append(tuple(Movement.Position))
         Movement.Last = "Left"
 
     def Right():
@@ -44,31 +67,15 @@ class Movement:
         Movement.Direction = Movement.Direction + 1
         if Movement.Direction > 3:
             Movement.Direction -= 4
-        log(Movement.Last)
         if Movement.Last == "Right":
-            log(Movement.Position)
-            API.setColor(Movement.Position[0] - 1, Movement.Position[1] - 1, "Y")
-            log("done")
+            Movement.History.append(tuple(Movement.Position))
         Movement.Last = "Right"
-
-def StartnFinish():
-    MazeSize = [API.mazeWidth(), API.mazeHeight()]
-    finish = [
-        [int(MazeSize[0] / 2), int(MazeSize[1] / 2)], 
-        [int(MazeSize[0] / 2 - 1), int(MazeSize[1] / 2)], 
-        [int(MazeSize[0] / 2), int(MazeSize[1] / 2 - 1)],
-        [int(MazeSize[0] / 2 - 1), int(MazeSize[1] / 2 - 1)]
-    ]
-    API.setText(0, 0, "abc")
-    for pos in finish:
-        API.setColor(pos[0], pos[1], "A")
-        API.setText(pos[0], pos[1], "END")
 
 def main():
     log("Running...")
-    StartnFinish()
+    MazeMapper.MazeSetup()
     while True:
-        log(f"{Movement.Position} [{Movement.Compas[Movement.Direction]}]")
+        log(f"[{Movement.Position[0] + 1}, {Movement.Position[1] + 1}] [{Movement.Compas[Movement.Direction]}]")
         if not API.wallLeft():
             Movement.Left()
         while API.wallFront():
