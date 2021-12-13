1a1fa2db2d42d42be2abcca5716ad0ca6bf3099a
Drakarta <Drakarta3@gmail.com>
2021-12-13 10:48:59 +0100

    Dictionary

--- mms-python-master/Main.py
+++ mms-python-master/Main.py
@@ -1,10 +1,11 @@
 import API
 import sys
+import json
 
 def log(string):
     sys.stderr.write("{}\n".format(string))
     sys.stderr.flush()
-
+    
 class MazeMapper:
     MazeSize = [API.mazeWidth(), API.mazeHeight()]
     Finish = [
@@ -13,47 +14,62 @@ class MazeMapper:
             [int(MazeSize[0] / 2), int(MazeSize[1] / 2 - 1)],
             [int(MazeSize[0] / 2 - 1), int(MazeSize[1] / 2 - 1)]
         ]
-    Map = []
+    Path = {}
+
+    def Write_Json(Data):
+        with open("Path.json", "w") as f:
+            json.dump(Data, f)
+
     def MazeSetup():
-        for x in range(MazeMapper.MazeSize[1]):
-            MazeMapper.Map.append([])
-            for y in range(MazeMapper.MazeSize[0]):
-                MazeMapper.Map[x].append(15)
-        log(MazeMapper.Map)
         API.setText(0, 0, "abc")
         for pos in MazeMapper.Finish:
             API.setColor(pos[0], pos[1], "A")
             API.setText(pos[0], pos[1], "END")
 
     def Mapper():
-        # 0 = No walls
-        # 1 = north wall
-        # 2 = east wall
-        # 4 = south wall
-        # 8 = west wall
-        # 15 = all wall
-        if API.wallLeft():
-            WallDirection = Movement.Direction - 1
-            if WallDirection < 0:
-                WallDirection += 4
-        if API.wallFront():
-            WallDirection = Movement.Direction
-        if API.wallRight():
-            WallDirection = Movement.Direction + 1
-            if WallDirection > 4:
-                WallDirection -= 4
-        Walls = 0
-        if WallDirection == 0:
-            Walls += 1
-        elif WallDirection == 1:
-            Walls += 2
-        elif WallDirection == 2:
-            Walls += 4
-        elif WallDirection == 3:
-            Walls += 8
-        MazeMapper.Map[Movement.Position[1]][Movement.Position[0]] = Walls
-        log(MazeMapper.Map)
-
+        Paths = []
+        if tuple(Movement.Position) not in Movement.History:
+            try:
+                Paths.append(Movement.History[-1])
+            except IndexError:
+                pass
+            if not API.wallLeft():
+                WallDirection = Movement.Direction - 1
+                if WallDirection < 0:
+                    WallDirection += 4
+                if WallDirection == 0:
+                    Paths.append((Movement.Position[0], Movement.Position[1] + 1))
+                elif WallDirection == 1:
+                    Paths.append((Movement.Position[0] + 1, Movement.Position[1]))
+                elif WallDirection == 2:
+                    Paths.append((Movement.Position[0], Movement.Position[1] - 1))
+                elif WallDirection == 3:
+                    Paths.append((Movement.Position[0] - 1, Movement.Position[1]))
+            if not API.wallFront():
+                WallDirection = Movement.Direction
+                if WallDirection == 0:
+                    Paths.append((Movement.Position[0], Movement.Position[1] + 1))
+                elif WallDirection == 1:
+                    Paths.append((Movement.Position[0] + 1, Movement.Position[1]))
+                elif WallDirection == 2:
+                    Paths.append((Movement.Position[0], Movement.Position[1] - 1))
+                elif WallDirection == 3:
+                    Paths.append((Movement.Position[0] - 1, Movement.Position[1]))
+            if not API.wallRight():
+                WallDirection = Movement.Direction + 1
+                if WallDirection > 4:
+                    WallDirection -= 4
+                if WallDirection == 0:
+                    Paths.append((Movement.Position[0], Movement.Position[1] + 1))
+                elif WallDirection == 1:
+                    Paths.append((Movement.Position[0] + 1, Movement.Position[1]))
+                elif WallDirection == 2:
+                    Paths.append((Movement.Position[0], Movement.Position[1] - 1))
+                elif WallDirection == 3:
+                    Paths.append((Movement.Position[0] - 1, Movement.Position[1]))
+            MazeMapper.Path[str(Movement.Position)] = Paths
+            MazeMapper.Write_Json(MazeMapper.Path)
+        
 class Movement:
     Compas = ["North", "East", "South", "West"]
     Position = [0, 0]
@@ -99,8 +115,8 @@ def main():
     log("Running...")
     MazeMapper.MazeSetup()
     while True:
-        log(f"[{Movement.Position[0] + 1}, {Movement.Position[1] + 1}] [{Movement.Compas[Movement.Direction]}]")
-        MazeMapper.Mapper   ()
+        log(f"[{Movement.Position[0]}, {Movement.Position[1]}] [{Movement.Compas[Movement.Direction]}]")
+        MazeMapper.Mapper()
         if not API.wallLeft():
             Movement.Left()
         while API.wallFront():
